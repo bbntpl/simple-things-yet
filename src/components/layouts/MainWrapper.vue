2d4e5c0ccee7dc85818c499c44e0832d59686e4e
bvrbryn445 <beaverbryanantipolo@gmail.com>
2023-10-18 23:28:03 -0400

    feat: back to top btn

--- src/components/layouts/MainWrapper.vue
+++ src/components/layouts/MainWrapper.vue
@@ -2,13 +2,20 @@
 	<div class="main">
 		<div class="container">
 			<slot></slot>
+			<el-backtop :right="100" :bottom="100" />
 		</div>
 	</div>
 </template>
 
 <script>
+import { ElBacktop } from 'element-plus';
+import '@/assets/styles/variables.css';
+
 export default {
 	name: 'MainWrapper',
+	components: {
+		ElBacktop,
+	},
 };
 </script>
 
@@ -28,5 +35,9 @@ export default {
 	align-items: center;
 	flex: 1 0 0;
 	width: 100%;
+}
+
+.el-backtop {
+	--el-backtop-text-color: var(--shuttle-gray-color);
 }
 </style>
